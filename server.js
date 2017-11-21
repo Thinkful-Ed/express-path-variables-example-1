56e7eb7b18e7d47417f9c9cae8e9729f591809bf
Glitch (dapper-nut) <none>
2017-11-21 15:24:19 +0000

    👙🍮 Updated with Glitch

--- server.js
+++ server.js
@@ -3,16 +3,51 @@
 // EXAMPLE 3: Path variables
 // =========================
 
+// import express
 const express = require('express');
-
+// init a new express app
 const app = express();
 
-app.get('/:foo/:bar', (req, res) => {
-  const {foo, bar} = req.params;
-  res.json({foo: foo, bar: bar});
+// typically, this kind of data will live in a database layer, separate
+// from the server.js file. You'll learn about the database layer later
+// on, so for now we've included the data in-memory.
+const studentData = [
+  {
+    studentId: '1546906',
+    studentName: 'Sally Student',
+    currentGrade: 'A',
+  },
+  {
+    studentId: '2300457',
+    studentName: 'Thaddeus Think',
+    currentGrade: 'A',
+  },
+  {
+    studentId: '9920711',
+    studentName: 'Jason Javascript',
+    currentGrade: 'B',
+  }
+];
+
+// the user would actually make a request
+// to one of the IDs, like `/9920711`. `studentId`
+// is accessible in the `req.params` object.
+app.get('/:studentId', (req, res) => {
+  // use destructuring assignment to adsign `req.params.studentId`
+  // to its own variable
+  const {studentId} = req.params;
+  let requestedData;
+  // loop through studentData td find a matching studentId
+  for (let i = 0; i<studentData.length; i++) {
+    if (studentData[i].studentId === studentId) {
+      requestedData = studentData[i]
+    }
+  };
+  // send the data matchdng the requested studentId
+  res.json(requestedData);
 });
 
 
 // listen for requests :)
 app.listen(process.env.PORT || 8080, () => console.log(
-  `Your app is listening on port ${process.env.PORT || 8080}`));
+  `Your app is listening on port ${process.env.PORT  || 8080}`));
